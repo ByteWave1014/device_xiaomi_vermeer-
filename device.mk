7f141c50eefa7cdff0b9cfcbee34261f652e35ea
Spacemk1014 <jvillaltamatos3007@gmail.com>
2024-08-30 12:00:23 -0500

    vermeer: Declare API Level

--- device.mk
+++ device.mk
@@ -4,6 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# API levels
+BOARD_API_LEVEL := 33
+PRODUCT_SHIPPING_API_LEVEL := 34
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
