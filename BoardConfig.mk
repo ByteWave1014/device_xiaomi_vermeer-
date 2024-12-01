61613c79d7da7bfcf1891058840db681d70fc843
Spacemk1014 <jvillaltamatos3007@gmail.com>
2024-12-01 16:51:47 -0500

    vermeer: Parchs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,34 +9,29 @@ KERNEL_PATH := device/xiaomi/vermeer-kernel
 
 # A/B
 AB_OTA_UPDATER := true
+
 AB_OTA_PARTITIONS += \
-    odm \
-    system \
     boot \
-    product \
-    vendor_dlkm \
     dtbo \
-    system_ext \
-    vendor_boot \
+    init_boot \
+    odm \
+    product \
     recovery \
+    system \
     system_dlkm \
+    system_ext \
+    vbmeta \
+    vbmeta_system \
     vendor \
-    init_boot
+    vendor_boot \
+    vendor_dlkm
 
 # Architecture
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-a
+TARGET_ARCH_VARIANT := armv9-a
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 := 
 TARGET_CPU_VARIANT := generic
-TARGET_CPU_VARIANT_RUNTIME := kryo300
-
-TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv7-a-neon
-TARGET_2ND_CPU_ABI := armeabi-v7a
-TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := generic
-TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a75
+TARGET_CPU_VARIANT_RUNTIME := cortex-a510
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := kalama
